5d2d252ee98fee2d38f2bc6f9647f879be3410ab
Daniel Henel <daniel-henel@protonmail.com>
2022-06-20 23:49:35 +0200

    database context

---
+++ LibraryService/Database/LibraryServiceContext.cs
@@ -0,0 +1,25 @@
+﻿using ClassLib;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Database
+{
+    public class LibraryServiceContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(localdb)\MSSQLLocalDB;Initial Catalog=LibraryService;");
+        }
+
+        public DbSet<Book> Books { get; set; }
+        public DbSet<User> Users { get; set; }
+        public DbSet<BookAuthor> Authors { get; set; }
+        public DbSet<BookCategory> Categories { get; set; }
+        public DbSet<HistoryItem> History { get; set; }
+
+    }
+}
