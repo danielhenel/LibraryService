3e0494f0549d9939131d2bc739db70aa933e6e9a
Daniel Henel <daniel-henel@protonmail.com>
2022-06-21 01:22:31 +0200

    service

---
+++ LibraryService/ServiceLib/Library.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ServiceLib
+{
+    public class LibraryService : ILibrary
+    {
+        public string GetData(int value)
+        {
+            Console.WriteLine("Received number {0}",value);
+            return string.Format("You entered: {0}", value);
+        }
+    }
+}
