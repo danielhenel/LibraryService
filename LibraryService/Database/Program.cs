4ccf1332826f5d54bc214b4dd7b64b4e16e67593
Daniel Henel <daniel-henel@protonmail.com>
2022-06-21 01:22:41 +0200

    host

--- LibraryService/Database/Program.cs
+++ LibraryService/Database/Program.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using ServiceLib;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel;
+using System.ServiceModel.Description;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,6 +13,31 @@ namespace Database
     {
         static void Main(string[] args)
         {
+            Uri baseAddress = new Uri("http://localhost:8000/LibraryService/");
+
+            ServiceHost selfHost = new ServiceHost(typeof(LibraryService), baseAddress);
+
+            try
+            {
+                selfHost.AddServiceEndpoint(typeof(ILibrary), new WSHttpBinding(), "LibraryService");
+                
+                ServiceMetadataBehavior smb = new ServiceMetadataBehavior();
+                smb.HttpGetEnabled = true;
+                selfHost.Description.Behaviors.Add(smb);
+                
+                selfHost.Open();
+                Console.WriteLine("The service is ready.");
+
+                Console.WriteLine("Press <Enter> to terminate the service.");
+                Console.WriteLine();
+                Console.ReadLine();
+                selfHost.Close();
+            }
+            catch (CommunicationException ce)
+            {
+                Console.WriteLine("An exception occurred: {0}", ce.Message);
+                selfHost.Abort();
+            }
         }
     }
 }
