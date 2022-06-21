9189bd5962ba9fe8308f15cc19b9ba9b998f11a8
Daniel Henel <daniel-henel@protonmail.com>
2022-06-21 23:34:10 +0200

    login form design

---
+++ LibraryService/UserApp/Login.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace UserApp
+{
+    partial class Login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.titlePanel = new System.Windows.Forms.Panel();
+            this.ciceroQuote = new System.Windows.Forms.Label();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.loginTextBox = new System.Windows.Forms.TextBox();
+            this.passwordTextBox = new System.Windows.Forms.TextBox();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.titlePanel.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // titlePanel
+            // 
+            this.titlePanel.BackColor = System.Drawing.Color.NavajoWhite;
+            this.titlePanel.Controls.Add(this.ciceroQuote);
+            this.titlePanel.Controls.Add(this.titleLabel);
+            this.titlePanel.Location = new System.Drawing.Point(1, 1);
+            this.titlePanel.Name = "titlePanel";
+            this.titlePanel.Size = new System.Drawing.Size(673, 146);
+            this.titlePanel.TabIndex = 1;
+            // 
+            // ciceroQuote
+            // 
+            this.ciceroQuote.AutoSize = true;
+            this.ciceroQuote.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ciceroQuote.Location = new System.Drawing.Point(229, 88);
+            this.ciceroQuote.Name = "ciceroQuote";
+            this.ciceroQuote.Size = new System.Drawing.Size(426, 16);
+            this.ciceroQuote.TabIndex = 4;
+            this.ciceroQuote.Text = "“A room without books is like a body without a soul.”, CICERO";
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.Location = new System.Drawing.Point(11, 31);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(235, 73);
+            this.titleLabel.TabIndex = 3;
+            this.titleLabel.Text = "Library";
+            // 
+            // loginTextBox
+            // 
+            this.loginTextBox.BackColor = System.Drawing.Color.AntiqueWhite;
+            this.loginTextBox.Location = new System.Drawing.Point(233, 198);
+            this.loginTextBox.Name = "loginTextBox";
+            this.loginTextBox.Size = new System.Drawing.Size(255, 20);
+            this.loginTextBox.TabIndex = 2;
+            this.loginTextBox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // passwordTextBox
+            // 
+            this.passwordTextBox.BackColor = System.Drawing.Color.AntiqueWhite;
+            this.passwordTextBox.Location = new System.Drawing.Point(233, 243);
+            this.passwordTextBox.Name = "passwordTextBox";
+            this.passwordTextBox.Size = new System.Drawing.Size(255, 20);
+            this.passwordTextBox.TabIndex = 3;
+            // 
+            // loginButton
+            // 
+            this.loginButton.BackColor = System.Drawing.Color.SandyBrown;
+            this.loginButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.loginButton.Location = new System.Drawing.Point(533, 198);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(91, 65);
+            this.loginButton.TabIndex = 4;
+            this.loginButton.Text = "Login";
+            this.loginButton.UseVisualStyleBackColor = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(143, 198);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "login";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(143, 243);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(74, 16);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "password";
+            // 
+            // Login
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Tan;
+            this.ClientSize = new System.Drawing.Size(675, 291);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.loginButton);
+            this.Controls.Add(this.passwordTextBox);
+            this.Controls.Add(this.loginTextBox);
+            this.Controls.Add(this.titlePanel);
+            this.Name = "Login";
+            this.Text = "Login";
+            this.titlePanel.ResumeLayout(false);
+            this.titlePanel.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel titlePanel;
+        private System.Windows.Forms.Label ciceroQuote;
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.TextBox loginTextBox;
+        private System.Windows.Forms.TextBox passwordTextBox;
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
