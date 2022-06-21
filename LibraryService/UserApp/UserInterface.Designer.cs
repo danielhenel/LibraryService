263d87171e7e7ffd4647317c9c6341861ee3c3b7
Daniel Henel <daniel-henel@protonmail.com>
2022-06-21 11:44:40 +0200

    user interface design

---
+++ LibraryService/UserApp/UserInterface.Designer.cs
@@ -0,0 +1,393 @@
+﻿namespace UserApp
+{
+    partial class UserInterface
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.titlePanel = new System.Windows.Forms.Panel();
+            this.loginButton = new System.Windows.Forms.Button();
+            this.ciceroQuote = new System.Windows.Forms.Label();
+            this.titleLabel = new System.Windows.Forms.Label();
+            this.notificationPanel = new System.Windows.Forms.FlowLayoutPanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.onLoanButton = new System.Windows.Forms.Button();
+            this.followedButton = new System.Windows.Forms.Button();
+            this.historyButton = new System.Windows.Forms.Button();
+            this.flowLayoutPanel2 = new System.Windows.Forms.FlowLayoutPanel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel4 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.panel5 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label5 = new System.Windows.Forms.Label();
+            this.panel6 = new System.Windows.Forms.Panel();
+            this.panel10 = new System.Windows.Forms.Panel();
+            this.followButton = new System.Windows.Forms.Button();
+            this.detailsButton = new System.Windows.Forms.Button();
+            this.panel9 = new System.Windows.Forms.Panel();
+            this.panel8 = new System.Windows.Forms.Panel();
+            this.panel7 = new System.Windows.Forms.Panel();
+            this.titlePanel.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.flowLayoutPanel2.SuspendLayout();
+            this.panel3.SuspendLayout();
+            this.panel4.SuspendLayout();
+            this.panel5.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.panel6.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // titlePanel
+            // 
+            this.titlePanel.BackColor = System.Drawing.Color.NavajoWhite;
+            this.titlePanel.Controls.Add(this.loginButton);
+            this.titlePanel.Controls.Add(this.ciceroQuote);
+            this.titlePanel.Controls.Add(this.titleLabel);
+            this.titlePanel.Controls.Add(this.notificationPanel);
+            this.titlePanel.Location = new System.Drawing.Point(1, 1);
+            this.titlePanel.Name = "titlePanel";
+            this.titlePanel.Size = new System.Drawing.Size(1247, 146);
+            this.titlePanel.TabIndex = 1;
+            // 
+            // loginButton
+            // 
+            this.loginButton.Location = new System.Drawing.Point(1154, 3);
+            this.loginButton.Name = "loginButton";
+            this.loginButton.Size = new System.Drawing.Size(75, 60);
+            this.loginButton.TabIndex = 5;
+            this.loginButton.UseVisualStyleBackColor = true;
+            // 
+            // ciceroQuote
+            // 
+            this.ciceroQuote.AutoSize = true;
+            this.ciceroQuote.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ciceroQuote.Location = new System.Drawing.Point(229, 88);
+            this.ciceroQuote.Name = "ciceroQuote";
+            this.ciceroQuote.Size = new System.Drawing.Size(426, 16);
+            this.ciceroQuote.TabIndex = 4;
+            this.ciceroQuote.Text = "“A room without books is like a body without a soul.”, CICERO";
+            // 
+            // titleLabel
+            // 
+            this.titleLabel.AutoSize = true;
+            this.titleLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.titleLabel.Location = new System.Drawing.Point(11, 31);
+            this.titleLabel.Name = "titleLabel";
+            this.titleLabel.Size = new System.Drawing.Size(235, 73);
+            this.titleLabel.TabIndex = 3;
+            this.titleLabel.Text = "Library";
+            // 
+            // notificationPanel
+            // 
+            this.notificationPanel.Location = new System.Drawing.Point(830, 69);
+            this.notificationPanel.Name = "notificationPanel";
+            this.notificationPanel.Size = new System.Drawing.Size(399, 63);
+            this.notificationPanel.TabIndex = 2;
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Tan;
+            this.panel1.Controls.Add(this.historyButton);
+            this.panel1.Controls.Add(this.followedButton);
+            this.panel1.Controls.Add(this.onLoanButton);
+            this.panel1.Location = new System.Drawing.Point(1, 153);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1247, 71);
+            this.panel1.TabIndex = 2;
+            // 
+            // onLoanButton
+            // 
+            this.onLoanButton.BackColor = System.Drawing.Color.OrangeRed;
+            this.onLoanButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.onLoanButton.Location = new System.Drawing.Point(265, 13);
+            this.onLoanButton.Name = "onLoanButton";
+            this.onLoanButton.Size = new System.Drawing.Size(235, 45);
+            this.onLoanButton.TabIndex = 0;
+            this.onLoanButton.Text = "On Load";
+            this.onLoanButton.UseVisualStyleBackColor = false;
+            // 
+            // followedButton
+            // 
+            this.followedButton.BackColor = System.Drawing.Color.SandyBrown;
+            this.followedButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.followedButton.Location = new System.Drawing.Point(506, 13);
+            this.followedButton.Name = "followedButton";
+            this.followedButton.Size = new System.Drawing.Size(235, 45);
+            this.followedButton.TabIndex = 1;
+            this.followedButton.Text = "Followed";
+            this.followedButton.UseVisualStyleBackColor = false;
+            // 
+            // historyButton
+            // 
+            this.historyButton.BackColor = System.Drawing.Color.SandyBrown;
+            this.historyButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.historyButton.Location = new System.Drawing.Point(747, 13);
+            this.historyButton.Name = "historyButton";
+            this.historyButton.Size = new System.Drawing.Size(235, 45);
+            this.historyButton.TabIndex = 2;
+            this.historyButton.Text = "History";
+            this.historyButton.UseVisualStyleBackColor = false;
+            // 
+            // flowLayoutPanel2
+            // 
+            this.flowLayoutPanel2.AutoScroll = true;
+            this.flowLayoutPanel2.BackColor = System.Drawing.Color.AntiqueWhite;
+            this.flowLayoutPanel2.Controls.Add(this.panel6);
+            this.flowLayoutPanel2.Font = new System.Drawing.Font("Ebrima", 8.25F);
+            this.flowLayoutPanel2.Location = new System.Drawing.Point(28, 275);
+            this.flowLayoutPanel2.Name = "flowLayoutPanel2";
+            this.flowLayoutPanel2.Size = new System.Drawing.Size(1190, 480);
+            this.flowLayoutPanel2.TabIndex = 6;
+            this.flowLayoutPanel2.Paint += new System.Windows.Forms.PaintEventHandler(this.flowLayoutPanel2_Paint);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Tan;
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button4.Location = new System.Drawing.Point(1, 226);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(24, 529);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "<";
+            this.button4.UseVisualStyleBackColor = false;
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Tan;
+            this.button5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button5.Location = new System.Drawing.Point(1224, 226);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(24, 529);
+            this.button5.TabIndex = 8;
+            this.button5.Text = ">";
+            this.button5.UseVisualStyleBackColor = false;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.Color.Tan;
+            this.panel3.Controls.Add(this.label4);
+            this.panel3.Controls.Add(this.panel4);
+            this.panel3.Controls.Add(this.panel5);
+            this.panel3.Controls.Add(this.panel2);
+            this.panel3.Location = new System.Drawing.Point(28, 230);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1190, 43);
+            this.panel3.TabIndex = 9;
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(729, 3);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(236, 36);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "End Date";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel4
+            // 
+            this.panel4.Controls.Add(this.label3);
+            this.panel4.Location = new System.Drawing.Point(487, 3);
+            this.panel4.Name = "panel4";
+            this.panel4.Size = new System.Drawing.Size(236, 36);
+            this.panel4.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(0, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(236, 36);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Category";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel5
+            // 
+            this.panel5.Controls.Add(this.label2);
+            this.panel5.Location = new System.Drawing.Point(3, 3);
+            this.panel5.Name = "panel5";
+            this.panel5.Size = new System.Drawing.Size(236, 36);
+            this.panel5.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(0, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(236, 36);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Title";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Location = new System.Drawing.Point(245, 3);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(236, 36);
+            this.panel2.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(0, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(236, 36);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Author";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // panel6
+            // 
+            this.panel6.Controls.Add(this.panel10);
+            this.panel6.Controls.Add(this.followButton);
+            this.panel6.Controls.Add(this.detailsButton);
+            this.panel6.Controls.Add(this.panel9);
+            this.panel6.Controls.Add(this.panel8);
+            this.panel6.Controls.Add(this.panel7);
+            this.panel6.Location = new System.Drawing.Point(3, 3);
+            this.panel6.Name = "panel6";
+            this.panel6.Size = new System.Drawing.Size(1187, 77);
+            this.panel6.TabIndex = 8;
+            // 
+            // panel10
+            // 
+            this.panel10.BackColor = System.Drawing.Color.SandyBrown;
+            this.panel10.Location = new System.Drawing.Point(726, 3);
+            this.panel10.Name = "panel10";
+            this.panel10.Size = new System.Drawing.Size(236, 71);
+            this.panel10.TabIndex = 9;
+            // 
+            // followButton
+            // 
+            this.followButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.followButton.Location = new System.Drawing.Point(1078, 10);
+            this.followButton.Name = "followButton";
+            this.followButton.Size = new System.Drawing.Size(79, 56);
+            this.followButton.TabIndex = 8;
+            this.followButton.Text = "Renew";
+            this.followButton.UseVisualStyleBackColor = true;
+            // 
+            // detailsButton
+            // 
+            this.detailsButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.detailsButton.Location = new System.Drawing.Point(993, 10);
+            this.detailsButton.Name = "detailsButton";
+            this.detailsButton.Size = new System.Drawing.Size(79, 56);
+            this.detailsButton.TabIndex = 7;
+            this.detailsButton.Text = "Details";
+            this.detailsButton.UseVisualStyleBackColor = true;
+            // 
+            // panel9
+            // 
+            this.panel9.BackColor = System.Drawing.Color.SandyBrown;
+            this.panel9.Location = new System.Drawing.Point(484, 3);
+            this.panel9.Name = "panel9";
+            this.panel9.Size = new System.Drawing.Size(236, 71);
+            this.panel9.TabIndex = 6;
+            // 
+            // panel8
+            // 
+            this.panel8.BackColor = System.Drawing.Color.SandyBrown;
+            this.panel8.Location = new System.Drawing.Point(242, 3);
+            this.panel8.Name = "panel8";
+            this.panel8.Size = new System.Drawing.Size(236, 71);
+            this.panel8.TabIndex = 5;
+            // 
+            // panel7
+            // 
+            this.panel7.BackColor = System.Drawing.Color.SandyBrown;
+            this.panel7.Location = new System.Drawing.Point(3, 3);
+            this.panel7.Name = "panel7";
+            this.panel7.Size = new System.Drawing.Size(236, 71);
+            this.panel7.TabIndex = 4;
+            // 
+            // UserInterface
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1254, 767);
+            this.Controls.Add(this.panel3);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.flowLayoutPanel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.titlePanel);
+            this.Name = "UserInterface";
+            this.Text = "UserInterface";
+            this.titlePanel.ResumeLayout(false);
+            this.titlePanel.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.flowLayoutPanel2.ResumeLayout(false);
+            this.panel3.ResumeLayout(false);
+            this.panel4.ResumeLayout(false);
+            this.panel5.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.panel6.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel titlePanel;
+        private System.Windows.Forms.Button loginButton;
+        private System.Windows.Forms.Label ciceroQuote;
+        private System.Windows.Forms.Label titleLabel;
+        private System.Windows.Forms.FlowLayoutPanel notificationPanel;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button historyButton;
+        private System.Windows.Forms.Button followedButton;
+        private System.Windows.Forms.Button onLoanButton;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Panel panel4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Panel panel5;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Panel panel6;
+        private System.Windows.Forms.Panel panel10;
+        private System.Windows.Forms.Button followButton;
+        private System.Windows.Forms.Button detailsButton;
+        private System.Windows.Forms.Panel panel9;
+        private System.Windows.Forms.Panel panel8;
+        private System.Windows.Forms.Panel panel7;
+    }
+}
