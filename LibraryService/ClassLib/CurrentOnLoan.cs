5199f2e119b30848e232e4318a831379d5364fdc
Daniel Henel <daniel-henel@protonmail.com>
2022-06-23 19:42:08 +0200

    CurrentOnLoad class

---
+++ LibraryService/ClassLib/CurrentOnLoan.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLib
+{
+    public class CurrentOnLoanBook
+    {
+        public int Id { get; set; }
+        public int UserId { get; set; }
+        public int BookId { get; set; }
+        public int numberOfRenews { get; set; }
+        public DateTime startDate { get; set; }
+
+        public DateTime endDate { get; set; }
+    }
+}
